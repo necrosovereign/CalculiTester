df2619486f73e95a4f0759b80e517b687b43adc9
Vladimir Kharchev <necrosovereign@gmail.com>
2018-03-10 21:55:15 +0300

    parsers for operators and shells

--- CTParser.hs
+++ CTParser.hs
@@ -51,6 +51,7 @@ leftParenT = char '(' *> pure ()
 rightParenT :: ReadP ()
 rightParenT = char ')' *> pure ()
 
+-- operator token
 operT :: ReadP String
 operT =
   tokenT isOperSymb
@@ -65,4 +66,62 @@ tokenizer =
       , fmap (const CloseP) rightParenT
       , fmap OperT operT]
     eof
-    return ts
+    pure ts
+
+-- tryP p tries p and returns Just when it succeeds and Nothing when it fails
+tryP :: ReadP a → ReadP (Maybe a)
+tryP p = fmap Just p <++ pure Nothing
+
+type Fixity = String → OperType
+
+-- parsers for operators of different fixity
+
+-- parses infix operator with fixity greater or equal to n
+infixOperT :: Fixity → Prec → ReadP (Maybe String)
+infixOperT fixity n =
+  tryP $ do
+  s ← operT
+  case fixity s of
+    Infix m | m >= n → pure s
+    _ → pfail
+
+prefixOperT :: Fixity → ReadP String
+prefixOperT fixity = do
+  s ← operT
+  case fixity s of
+    Prefix → pure s
+    _ → pfail
+
+postfixOperT :: Fixity → ReadP String
+postfixOperT fixity = do
+  s ← operT
+  case fixity s of
+    Postfix→ pure s
+    _ → pfail
+
+-- full record parser
+recordP :: ReadP (Rec String)
+recordP = undefined
+
+-- parses a nucleus: variable, atom or a record in parenthesis
+nucleusP :: ReadP (Rec String)
+nucleusP = choice
+  [ fmap Var varT
+  , fmap (\x → Atom x []) atomT
+  , between leftParenT rightParenT recordP
+  ]
+
+-- parses a shell: a nucleus, surrounded by prefix and postfix operator
+shellP :: Fixity → ReadP (Rec String)
+shellP fixity = do
+  prefs ← many (prefixOperT fixity)
+  nucl ← nucleusP
+  postf ← many (postfixOperT fixity)
+  pure $
+          foldr (\op x → Atom op [x])
+          (foldl (\x op → Atom op [x]) nucl postf)
+          prefs
+
+-- parses a record without infix operators
+simpleRecordP :: ReadP (Rec String)
+simpleRecordP = undefined
